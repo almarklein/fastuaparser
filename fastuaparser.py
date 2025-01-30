9b2d47f316a33137028b89a81c63eaef03c207fe
Almar Klein <almar@almarklein.org>
2025-01-30 12:53:32 +0100

    Modernize repo a bit

--- fastuaparser.py
+++ fastuaparser.py
@@ -4,7 +4,7 @@
 __version__ = "0.1.3"
 
 
-def parse_ua(s, always_include_os=False):
+def parse_ua(s: str, always_include_os: bool=False) -> str:
     """ Parse the given user-agent string and return a string of the
     form "client - os". Use x.partition(' - ') if needed. The
     client is "Browser" if unknown. The os is "Other" if unknown and
